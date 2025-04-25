f322b5f09df1cc803a6b980e2f3d1f57071051af
AdarshFusion <chauhanadarsh432@gmail.com>
2025-04-25 16:39:11 +0530

    update

---
+++ 01-Base-of-js.js
@@ -0,0 +1,8 @@
+// What is JavaScript?
+
+// JavaScript is the programming language of the web.
+// It can update and change both HTML and CSS.
+// It can calculate, manipulate and validate data.
+// We use it to give instructions to the computer.
+
+// input(code) ---> computer --> Output
