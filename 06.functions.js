7ead3e123a1d819bfb798898207a41b6c7e917c8
AdarshFusion <chauhanadarsh432@gmail.com>
2025-04-26 20:36:32 +0530

    update

---
+++ 06.functions.js
@@ -0,0 +1,21 @@
+/*
+
+Function
+- A JavaScript function is a block of code designed to perform a particular task.
+- A JavaScript function is executed when "something" invokes it (calls it).
+
+Syntax: 
+        function name(parameter1, parameter2, parameter3) {
+            // code to be executed
+        }
+
+*/
+
+// Example-1: Calculate the product of two numbers, and return the result:
+// Function is called, the return value will end up in x
+let x = myFunction(4, 3);
+
+function myFunction(a, b) {
+    // Function returns the product of a and b
+    return a * b;
+}
